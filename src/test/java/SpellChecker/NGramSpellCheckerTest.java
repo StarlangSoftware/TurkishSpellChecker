c8fc378e3b654f92e50c90a9c537407a6faeaa44
barbaros-durusoy <barbaros.durusoy@gmail.com>
2022-11-16 02:09:38 +0300

    Added new tests, modified existing ones

--- src/test/java/SpellChecker/NGramSpellCheckerTest.java
+++ src/test/java/SpellChecker/NGramSpellCheckerTest.java
@@ -23,7 +23,7 @@ public class NGramSpellCheckerTest {
     public void testSpellCheck() {
         Sentence[] original = {new Sentence("demokratik cumhuriyet en kıymetli varlığımızdır"),
                 new Sentence("bu tablodaki değerler zedelenmeyecektir"),
-                new Sentence("vakfın geleneksel yılın sporcusu anketi 43. yaşını doldurdu"),
+                new Sentence("vakfın geleneksel yılın sporcusu anketi yeni yaşını doldurdu"),
                 new Sentence("demokrasinin icadı bu ayrımı bulandırdı"),
                 new Sentence("dışişleri müsteşarı Öymen'in 1997'nin ilk aylarında Bağdat'a gitmesi öngörülüyor"),
                 new Sentence("büyüdü , palazlandı , devleti ele geçirdi"),
@@ -33,20 +33,20 @@ public class NGramSpellCheckerTest {
                 new Sentence("son derece kısıtlı kelimeler çerçevesinde kendilerini uzun cümlelerle ifade edebiliyorlar"),
                 new Sentence("minibüs durağı"),
                 new Sentence("noter belgesi"),
-                new Sentence("")};
-        Sentence[] modified = {new Sentence("demokratik cumhüriyet rn kımetli varlıgımızdır"),
+                new Sentence("bu filmi daha önce görmemiş miydik diye sordu")};
+        Sentence[] modified = {new Sentence("demokratik cumhüriyet en kımetli varlıgımızdır"),
                 new Sentence("bu tblodaki değerler zedelenmeyecektir"),
-                new Sentence("vakfın geeneksel yılin spoşcusu ankşti 43. yeşını doldürdu"),
-                new Sentence("demokrasinin icşdı buf ayrmıı bulandürdı"),
+                new Sentence("vakfın geeneksel yılin spoşcusu ankşti yeni yeşını doldürdu"),
+                new Sentence("demokrasinin icşdı bu ayrmıı bulandürdı"),
                 new Sentence("dışişleri mütseşarı Öymen'in 1997'nin iljk aylğrında Bağdat'a gitmesi öngörülüyor"),
-                new Sentence("büyüdü , palazandı , devltei eöe geçridi"),
+                new Sentence("büyüdü , palazandı , devltei ele geçridi"),
                 new Sentence("her makenin cültte aklma sürdsi farlkıdır"),
-                new Sentence("yılın sno ayında 10 gazteci gözlatına alündı"),
-                new Sentence("iki piotun kulçandığı uçkata üir hotes görçv alyıor"),
+                new Sentence("yılın son ayında 10 gazteci gözlatına alündı"),
+                new Sentence("iki piotun kulçandığı uçkata bir hotes görçv alyıor"),
                 new Sentence("son deece kısütlı keilmeler çeçevesinde kendülerini uzuü cümllerle ifüde edbeiliyorlar"),
                 new Sentence("minibü durağı"),
                 new Sentence("ntoer belgesi"),
-                new Sentence("")};
+                new Sentence("bu filmi daha önce görmemişmiydik diye sordu")};
         NGramSpellChecker nGramSpellChecker = new NGramSpellChecker(fsm, nGram, true);
         for (int i = 0; i < modified.length; i++){
             assertEquals(original[i].toString(), nGramSpellChecker.spellCheck(modified[i]).toString());
@@ -86,7 +86,7 @@ public class NGramSpellCheckerTest {
                 new Sentence("4 lı tahıl zirvesi İstanbul'da gerçekleşti"),
                 new Sentence("10 lük sistemden 100 lık sisteme geçiş yapılacak"),
                 new Sentence("play - off maçlarına çıkacak takımlar belli oldu"),
-                new Sentence("bu sno model ciha 24inç ekran büyüklüğünde ve 9kg ağırlıktadır")};
+                new Sentence("bu son model ciha 24inç ekran büyüklüğünde ve 9kg ağırlıktadır")};
         NGramSpellChecker nGramSpellChecker = new NGramSpellChecker(fsm, nGram, true);
         for (int i = 0; i < modified.length; i++){
             assertEquals(original[i].toString(), nGramSpellChecker.spellCheck(modified[i]).toString());
