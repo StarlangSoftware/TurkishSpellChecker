4f66c1379bad4e7fdf3b80ec2684d968ab0ee3f8
barbaros-durusoy <barbaros.durusoy@gmail.com>
2023-01-23 04:50:11 +0300

    Added new operators, removed unused ones, added comments

--- src/main/java/SpellChecker/Operator.java
+++ src/main/java/SpellChecker/Operator.java
@@ -1,5 +1,31 @@
 package SpellChecker;
 
+/**
+ * Enum class that represents different types of spell checking operators that can be applied to a Word.
+ */
 public enum Operator {
-    NO_CHANGE, MISSPELLED_REPLACE, FORCED_MERGE, FORCED_SPLIT, SPLIT_WITH_SHORTCUT, SPELL_CHECK, SPLIT, FORWARD_MERGE, BACKWARD_MERGE
+
+    /** No change is made to the Word. */
+    NO_CHANGE,
+
+    /** The Word is changed into a Word in the misspellings list */
+    MISSPELLED_REPLACE,
+
+    /** The Word is changed into a Candidate by deleting, adding, replacing a character or swapping two consecutive characters. */
+    SPELL_CHECK,
+
+    /** The Word is split into multiple Candidates. */
+    SPLIT,
+
+    /** The Word and the Word after are merged into one Candidate. */
+    FORWARD_MERGE,
+
+    /** The Word and the Word before are merged into one Candidate. */
+    BACKWARD_MERGE,
+
+    /** The Word is changed into a Candidate based on the context based spell checking algorithm. */
+    CONTEXT_BASED,
+
+    /** The Word is changed into a Candidate based on the trie based spell checking algorithm. */
+    TRIE_BASED
 }
